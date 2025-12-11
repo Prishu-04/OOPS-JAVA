63b6cc074324b7ddaf7112387955f24fda90edc0
Prishu-04 <pratyakshsrivastava@23ksgmail.com>
2025-12-11 16:30:23 +0530

    Added

---
+++ Lab1/displaynr.java
@@ -0,0 +1,8 @@
+class displaynr{
+     public static void main(String[] args){
+          System.out.println("Name - Pratyaksh\n");
+          System.out.println("Rollno - 24155040\n");
+          System.out.println("Section - CSE-48\n");
+          System.out.println("Branch - CSE (AI & ML)\n");
+     }
+}
